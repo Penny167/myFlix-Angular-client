ab31f5a9fb427486ff9f033bab0480e2be1f91e0
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-12-20 20:38:33 +0000

    Added function to open login modal when login button clicked on app view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -13,12 +14,13 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-  // This function opens the dialog when the registration button is clicked  
+  // This function opens the registration dialog when the registration button is clicked  
   openUserRegistrationDialog(): void {
-      this.dialog.open(UserRegistrationFormComponent, {
-  // Assigning the dialog a width
-      width: '280px'
-      });
-    }
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+  }
+// This function opens the login dialog when the login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, { width: '280px' });
+  }  
 
 }

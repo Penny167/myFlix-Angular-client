68e3c1ee941f8acca80f4f1624be444d743cbe53
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-12-20 14:12:08 +0000

    Imported dialog and registration component to app.ts file and created function to open the dialog to display the registration component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  // This function opens the dialog when the registration button is clicked  
+  openUserRegistrationDialog(): void {
+      this.dialog.open(UserRegistrationFormComponent, {
+  // Assigning the dialog a width
+      width: '280px'
+      });
+    }
+
 }

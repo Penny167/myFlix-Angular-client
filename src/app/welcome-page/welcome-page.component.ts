a2e17638fcb92f550cfeada735eb8399441cce24
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-12-29 17:22:32 +0000

    Cut code to implement login and registration view from app component and moved to welcome page component.

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+// import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
+
 export class WelcomePageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void { }
+  // This function opens the registration dialog when the registration button is clicked  
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
   }
-
+  // This function opens the login dialog when the login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, { width: '280px' });
+  } 
+  /* This function opens the all movies dialog when the all movies button is clicked
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, { width: '500px' });
+  } */  
 }

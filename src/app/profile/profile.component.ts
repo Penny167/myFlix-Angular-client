e9a23cdf6b45ef6045d7f57c0ab1dcb278ab776f
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-12-31 14:28:26 +0000

    Updated profile component to get user data using api service and added function to open dialog to edit user profile.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditProfileFormComponent } from '../edit-profile-form/edit-profile-form.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
+
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: { } = { }; // Set user to an empty object
+  username = localStorage.getItem('user'); // Username needed to make request to getUser Api endpoint
+  
+  constructor(public fetchApiData: FetchApiDataService, public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { this.getProfile() } // We want the data to populate the template as soon as the component loads
+
+  getProfile(): void {
+    this.fetchApiData.getUser(this.username!).subscribe((resp: any) => { this.user = resp });
+  }
+
+  openEditProfileFormDialog(): void {
+    this.dialog.open(EditProfileFormComponent, { width: '280px' });
   }
 
 }

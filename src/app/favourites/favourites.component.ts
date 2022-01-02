64c1f56654b239d365268930ad7193edb586ffe8
Penny167 <78602066+Penny167@users.noreply.github.com>
2022-01-02 15:03:27 +0000

    Created deleteFavouriteMovie function to remove a movie from favourites. Changed mat-icon in to a button and used event binding to invoke the deleteFavouriteMovie function in the component when the button is clicked in the template.

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { GenreComponent } from '../genre/genre.component';
 import { DirectorComponent } from '../director/director.component';
 import { SynopsisComponent } from '../synopsis/synopsis.component';
@@ -16,7 +17,7 @@ export class FavouritesComponent implements OnInit {
   favourites: any[] = []; // Set favourites equal to empty array
   username = localStorage.getItem('user'); // Username needed to make requests to Api endpoints
 
-  constructor(public fetchApiData: FetchApiDataService, public dialog: MatDialog) { }
+  constructor(public fetchApiData: FetchApiDataService, public dialog: MatDialog, public snackBar: MatSnackBar) { }
 
   ngOnInit(): void { this.getFavouriteMovies() }
 
@@ -42,6 +43,18 @@ export class FavouritesComponent implements OnInit {
     this.dialog.open(SynopsisComponent, {
       data: { description: description },
       width: '280px' 
+    });
+  }
+
+  deleteFavouriteMovie(movieID: string, title: string): void {
+    this.fetchApiData.deleteFavourite(this.username!, movieID).subscribe((resp: any) => { 
+      this.favourites = resp;
+      this.snackBar.open(`${title} has been removed from favourites!`, 'OK', { duration: 4000 });
+     }, (result) => {
+       console.log(result);
+       this.snackBar.open(`Hmm, we couldn't unfavourite ${title}. Please try again`, 'OK', {
+         duration: 4000
+       }); 
     });
   }
 

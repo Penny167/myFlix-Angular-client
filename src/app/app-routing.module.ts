3a50f08b4b3e2531ce4f75b91ad269640086ce29
Penny167 <78602066+Penny167@users.noreply.github.com>
2022-01-03 13:39:45 +0000

    Imported navigation component in to routing module and changed movies path to route to this component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
+// import { MovieCardComponent } from './movie-card/movie-card.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
-  { path: 'movies', component: MovieCardComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' }
+//  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+  { path: 'movies', component: NavigationComponent },
 ];
 
 @NgModule({

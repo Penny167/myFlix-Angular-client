2421d04fd9a2bea71c3c2eab93dc9ac35cfb91d7
Penny167 <78602066+Penny167@users.noreply.github.com>
2022-01-03 17:39:55 +0000

    Implemented responsive side navigation in navigation template. Added comments to navigation component. Added color property to welcome component toolbar to be consistent with navigation view.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -8,10 +8,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-
+// Set isHandset variable using observe method on the BreakpointObserver so we can use this in the template to change the layout for mobile screen sizes
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
-
-  constructor(private breakpointObserver: BreakpointObserver) { }
+// A BreakpointObserver is added as a property so that we can use its methods within the class to implement layout changes for mobile screen sizes
+  constructor(private breakpointObserver: BreakpointObserver) { } 
 
   ngOnInit(): void {
   }

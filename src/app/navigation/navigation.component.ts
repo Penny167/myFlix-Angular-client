559e86aa7709655ee759ffbde6ff49bfb584cbcc
Penny167 <78602066+Penny167@users.noreply.github.com>
2022-01-04 12:06:47 +0000

    Implemented responsive sidenav with toolbar for desktop and buttons that change the content component. ngSwitch used to select the view based on the button selected. Updated date in login component using pipe to change how it displays.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -11,9 +11,13 @@ export class NavigationComponent implements OnInit {
 // Set isHandset variable using observe method on the BreakpointObserver so we can use this in the template to change the layout for mobile screen sizes
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
 // A BreakpointObserver is added as a property so that we can use its methods within the class to implement layout changes for mobile screen sizes
+  
+  componentDisplayed: String = 'movies';
+
   constructor(private breakpointObserver: BreakpointObserver) { } 
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+
+  changeComponentDisplayed(name: string): void { this.componentDisplayed = name }
 
 }

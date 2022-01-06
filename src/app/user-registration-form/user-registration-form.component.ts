1a35c95bcd59958d248551bd2c043be78a5ac426
Penny167 <78602066+Penny167@users.noreply.github.com>
2022-01-06 18:54:58 +0000

    Edited comments on user-registration files and removed unnecessary css.

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog'; // Used to create a reference to the dialog that we open in the app component so we can close it here if registration is successful
+import { MatDialogRef } from '@angular/material/dialog'; // Used to create a reference to the dialog that we open in the welcome-page component so we can close it here if registration is successful
 import { FetchApiDataService } from '../fetch-api-data.service'; // We will use the userRegistration function created on this service to register a user in our registerUser function
 import { MatSnackBar } from '@angular/material/snack-bar'; // Used to create pop-up notifications to the user
 
@@ -10,10 +10,10 @@ import { MatSnackBar } from '@angular/material/snack-bar'; // Used to create pop
 })
 
 export class UserRegistrationFormComponent implements OnInit {
-
+// userData values will be populated by using the ngModel directive on form inputs in the user-registration template
   userData = { Username: '', Password: '', Email: '', Birthday: '' };
   
-  constructor( // Passing these parameters sets them as properties on each instance of the class
+  constructor( // Passing these classes as parameters to the constructor sets them as properties on the class that we can then access as needed
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar 
@@ -23,10 +23,8 @@ export class UserRegistrationFormComponent implements OnInit {
 
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => { // invokes the userRegistration function in our Api service, passing the userData
-  // Logic for a successful user registration goes here
-     this.dialogRef.close(); // Closes the dialog opened when the user clicks registration button in the app component
-     console.log(result);
-     this.snackBar.open(`Welcome to myFlix ${this.userData.Username}! Log in to start browsing movies`, 'Cool!', { // Message pops up to confirm successful registration
+     this.dialogRef.close(); // Closes the dialog opened in the welcome-page component component
+     this.snackBar.open(`Welcome to myFlix ${this.userData.Username}! Log in to start browsing movies.`, 'Cool!', { // Message pops up to confirm successful registration
         duration: 4000
      });
     }, (result) => {

458d06e629b5c8dafbb5ebdb66890df6e5cf26c4
Penny167 <78602066+Penny167@users.noreply.github.com>
2022-01-07 00:46:30 +0000

    Added comments to app-routing-module file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-// import { MovieCardComponent } from './movie-card/movie-card.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
-//  { path: 'movies', component: MovieCardComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
-  { path: 'movies', component: NavigationComponent },
+  { path: 'movies', component: NavigationComponent } // Movies is the default view inside the navigation component
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], // Creates in-app navigation using the routes defined above
   exports: [RouterModule]
 })
 
